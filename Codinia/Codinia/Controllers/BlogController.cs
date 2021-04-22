bb6c27fa662d274f8316570bbef6c9313fd3a611
rai hamza <hamzarai595@gmail.com>
2021-04-22 11:58:28 +0500

    blog work

---
+++ Codinia/Codinia/Controllers/BlogController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Codinia.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Blog()
+        {
+            return View("Blog");
+        }
+        public IActionResult EightTips()
+        {
+            return View();
+        }
+        public IActionResult ElevenTips()
+        {
+            return View();
+        }
+        public IActionResult BlogPost()
+        {
+            return View();
+        }
+        public IActionResult TenTips()
+        {
+            return View();
+        }
+    }
+}
---
+++ Codinia/Codinia/Models/FormModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Codinia.Models
+{
+    public class FormModel
+    {
+        [Required]
+        public string  name { get; set; }
+        [Required]
+        public string email { get; set; }
+        [Required]
+        public string message { get; set; }
+    }
+}
